4e6c4cf9318d01737da75655c3c45d36fee1b502
KobakhidzeDimitri1 <dimitri.kobakhidze.1@btu.edu.ge>
2023-12-09 20:03:33 +0400

    initial

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
 import './App.css';
+
+import imageDesktop from "./images/image-product-desktop.jpg" 
+import imageMobile from "./images/image-product-mobile.jpg" 
+import cartIcon from "./images/icon-cart.svg"
+
+const mobileSize = window.innerWidth <= 600
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <main>
+        <img className='main-pic' src={mobileSize ? imageMobile : imageDesktop} alt='chanel-parfume' />
+        <section className='product-info-container'>
+          <span className='product-heading'>Perfume</span>
+          <h1>Gabrielle Essence Eau De Parfum</h1>
+          <p>
+            A floral, solar and voluptuous interpretation composed by Olivier Polge, Perfumer-Creator for the House of CHANEL
+          </p>
+          <div className='product-price'>
+            <span className='current-price'>$149.99</span>
+            <span className='prev-price'>$169.99</span>
+          </div>
+          <button className='add-button'>
+            <img src={cartIcon} alt='cart-icon' />
+            Add to Cart
+          </button>
+        </section>
+      </main>
     </div>
   );
 }
